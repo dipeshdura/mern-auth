2f520d540c995306ee175046f893b163fcd9eac2
dipeshdura <dipeshdura14@gmail.com>
2024-06-04 21:38:01 +0545

    complete sign in page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
-import Signin from './pages/Signin';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
-      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/profile' element={<Profile/>}/>
     </Routes>

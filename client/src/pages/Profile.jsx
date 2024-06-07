c79079ab3993b238f9a0c04f02fb4e16421d9c4d
dipeshdura <dipeshdura14@gmail.com>
2024-06-07 15:58:01 +0545

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,7 +11,10 @@ import { useDispatch } from 'react-redux';
 import { 
   updateUserStart,
   updateUserSuccess,
-  updateUserFailure 
+  updateUserFailure, 
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure
 } from '../redux/user/userSlice';
 const Profile = () => {
   const dispatch =useDispatch();
@@ -72,6 +75,22 @@ const Profile = () => {
     } catch (error) {
       dispatch(updateUserFailure(error))
     }
+  };
+  const handleDeleteAccount =async ()=>{
+    try {
+      dispatch(deleteUserStart());
+      const res =await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:"DELETE",
+      });
+      const data =await res.json();
+      if(data.success ===false){
+        dispatch(deleteUserFailure(data))
+        return; 
+      }
+      deleteUserSuccess(data);
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
   }
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -122,7 +141,7 @@ const Profile = () => {
         <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading?'Loading...':'Update'}</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span onClick={handleDeleteAccount} className='text-red-700 cursor-pointer'>Delete Account</span>
 
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
